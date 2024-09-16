1b12d61bf9ae0c7a2d7aa10f4a3283c4fbe25678
zwangsfixiert <dominik.ennser@gmail.com>
2024-09-16 23:07:26 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,144 +1,61 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-  
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  description = "NixOS configuration with Hyprland and LightDM";
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Europe/Vienna";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "de_AT.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_AT.UTF-8";
-    LC_IDENTIFICATION = "de_AT.UTF-8";
-    LC_MEASUREMENT = "de_AT.UTF-8";
-    LC_MONETARY = "de_AT.UTF-8";
-    LC_NAME = "de_AT.UTF-8";
-    LC_NUMERIC = "de_AT.UTF-8";
-    LC_PAPER = "de_AT.UTF-8";
-    LC_TELEPHONE = "de_AT.UTF-8";
-    LC_TIME = "de_AT.UTF-8";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  outputs = { nixpkgs, flake-utils, home-manager, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations.my-nixos-config = pkgs.lib.nixosSystem {
+        system = "x86_64-linux";  # oder aarch64-linux je nach Architektur
 
-  # Enable the Hyprland Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  programs.hyprland.enable = true;
-  
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "de";
-    variant = "nodeadkeys";
-  };
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+          # Konfiguration für Hyprland und LightDM
+          {
+            environment.systemPackages = with pkgs; [
+              hyprland
+              hyprpaper
+              waybar
+              wofi
+              xwayland
+              kitty   # Optional
+              networkmanagerapplet
+            ];
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+            # LightDM als Display Manager aktivieren
+            services.xserver = {
+              enable = true;
+              layout = "de";  # Tastaturlayout
+              displayManager.lightdm = {
+                enable = true;
+                greeters = [ pkgs.lightdm-mini-greeter ];  # Minimaler Greeter für LightDM
+              };
+              desktopManager.default = "none";  # Deaktiviert den Standard-Desktop-Manager
+              windowManager.hyprland.enable = true;  # Hyprland als WM aktivieren
+            };
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+            services.pipewire = {
+              enable = true;
+              alsa.enable = true;
+              pulse.enable = true;
+            };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.strizzi = {
-    isNormalUser = true;
-    description = "strizzi";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      geany
-      thunderbird
-    ];
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    git
-    wget
-    neovim
-    hyprland
-    waybar
-    swaylock
-    xwayland
-    wofi
-    kitty # Terminal-Emulator
-    gnome.nautilus
-    xdg-desktop-portal-wlr
-    networkmanagerapplet
-    home-manager
-    ags
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+            # Optionale Konfiguration für LightDM, um Hyprland korrekt zu starten
+            programs.lightdm.settings = {
+              user-session = "hyprland";
+              minimum-vt = 7;
+            };
+          }
+        ];
+      };
+    });
 }
-
